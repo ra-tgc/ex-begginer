20e3594573215ee5076a4ce211064f23fbaaab91
ra-tgc <masaki.taguchi@rakus.co.jp>
2021-05-20 17:58:09 +0900

    On the way

--- src/main/java/com/example/controller/ShoppingCartController.java
+++ src/main/java/com/example/controller/ShoppingCartController.java
@@ -1,10 +1,17 @@
 package com.example.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Item;
 
 @Controller
 @RequestMapping("/exam06")
@@ -14,17 +21,34 @@ public class ShoppingCartController {
 	private ServletContext application;
 
 	@RequestMapping("")
-	public String index() {
+	public String index(Model model) {
+		Map<Integer, Item> itemMap = new HashMap<>();
+		int sumPrice = 0;
+
+		itemMap.put(1, new Item("手帳ノート", 1000));
+		itemMap.put(2, new Item("文房具セット", 1500));
+		itemMap.put(3, new Item("ファイル", 2000));
+
+		application.setAttribute("itemMap", itemMap);
+
+		@SuppressWarnings("unchecked")
+		List<Item> itemListInCart = (List<Item>) session.getAttribute("cartItem");
+		for (Item item : itemListInCart) {
+			sumPrice += item.getPrice();
+		}
+
+		model.addAttribute("sumPrice", sumPrice);
+
 		return "item-and-cart";
 	}
 
 	@RequestMapping("in-cart")
-	public String inCart() {
-		return "item-and-cart";
+	public String inCart(Model model) {
+		return index(model);
 	}
 
 	@RequestMapping("delete")
-	public String delete() {
-		return "item-and-cart";
+	public String delete(Model model) {
+		return index(model);
 	}
 }
--- src/main/java/com/example/domain/Item.java
+++ src/main/java/com/example/domain/Item.java
@@ -4,6 +4,14 @@ public class Item {
 	private String name;
 	private Integer price;
 
+	public Item() {
+	}
+
+	public Item(String name, Integer price) {
+		this.name = name;
+		this.price = price;
+	}
+
 	@Override
 	public String toString() {
 		return "Item [name=" + name + ", price=" + price + "]";
