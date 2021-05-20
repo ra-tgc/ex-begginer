eac3fcce5aab8df6e659140455b0b6eaaf6b3227
ra-tgc <masaki.taguchi@rakus.co.jp>
2021-05-20 15:50:25 +0900

    made exam04 user form

--- src/main/java/com/example/form/UserForm.java
+++ src/main/java/com/example/form/UserForm.java
@@ -1,5 +1,45 @@
 package com.example.form;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class UserForm {
+	@NotBlank(message = "名前は必須です")
+	private String name;
+	@NotBlank(message = "年齢は必須です")
+	@Pattern(regexp = "^[0-9]*$")
+	private String age;
+	@NotBlank(message = "コメントは必須です")
+	@Size(min = 1, max = 256, message = "コメントは1文字以上256文字以内で記載してください")
+	private String comment;
+
+	public Integer getIntAge() {
+		return Integer.parseInt(age);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
 
 }
